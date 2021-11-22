fc170ee0fbedf9743bf7fb8a0a97ae7f51045112
DJNIMUTHU <94827795+DJNIMUTHU@users.noreply.github.com>
2021-11-22 09:54:32 +0000

    Delete DewmiBot directory

--- DewmiBot/modules/yt.py
+++
@@ -1,6 +0,0 @@
-__help__ = """
-@szrosebot🇱🇰
- ❍ /video <text>: perform a youtube search
- ❍ /song <link> or /video <link>: Downlods a video or audio from a youtube video to the bots local server
-"""
-__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ📺"
